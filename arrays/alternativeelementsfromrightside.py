0917a08831cd37a790083a68910618309f1823fa
Prabha <spprabha87@gmail.com>
2025-11-20 22:44:20 +0100

    alternativeelementscode added

---
+++ arrays/alternativeelementsfromrightside.py
@@ -0,0 +1,20 @@
+arr = [1,2,3,4,5,6,7,8,9,10]
+print(arr)
+for i in range(len(arr)-1,-1,-2):
+    print(arr[i])
+
+
+arr = [1,2,3,4,5,6,7,8,9,10,12,334]
+print(arr)
+def readalternative(arr):
+    result = []
+    for i in range(len(arr)-1,-1,-2):
+        result.append(arr[i])
+    return result
+
+print(readalternative(arr))
+
+
+
+
+arras = []
